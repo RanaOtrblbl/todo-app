3bd09ebcb40f1ce73cf17ef08d6d63348b960343
RanaOtrblbl <ranaotrblbl@gmail.com>
2025-08-05 14:22:35 +0300

    ActionText eklendi

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,12 @@
 module TodosHelper
-end
+    def priority_bg_class(priority)
+      {
+        1 => 'bg-red-100',
+        2 => 'bg-orange-100',
+        3 => 'bg-yellow-100',
+        4 => 'bg-green-100',
+        5 => 'bg-blue-100'
+      }[priority] || 'bg-white'
+    end
+  end
+  
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,4 @@
 class Todo < ApplicationRecord
     belongs_to :project
+    has_rich_text :description
 end
