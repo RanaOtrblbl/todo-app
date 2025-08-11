4a7d7aa9b033839e0d1aaea71840dc54cde366b1
RanaOtrblbl <ranaotrblbl@gmail.com>
2025-08-11 15:54:41 +0300

    tailwind düzenlemeleri yapıldı

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,18 +1,17 @@
 module TodosHelper
     def priority_bg_class(priority)
       {
-        1 => 'bg-red-100',
-        2 => 'bg-orange-100',
-        3 => 'bg-yellow-100',
-        4 => 'bg-green-100',
-        5 => 'bg-blue-100'
+        1 => 'bg-teal-100',
+        2 => 'bg-indigo-100',
+        3 => 'bg-purple-100',
+        4 => 'bg-red-100',
+        5 => 'bg-yellow-100'
       }[priority] || 'bg-white'
     end
 
-    
       def todo_deadline_class(todo)
         if todo.deadline.present? && todo.deadline <= Date.today
-          "border-2 border-red-500 bg-red-50"
+          "border-2 border-red-500 bg-red-10"
         else
           "border border-gray-200"
         end
